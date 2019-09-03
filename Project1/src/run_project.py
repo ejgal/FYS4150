775518be44077b89e2a029e14894f3bcdbc8fb24
Eirik Gallefoss <itsjef@radiorakel.no>
2019-09-03 10:21:28 +0200

    Add data and figures output + add dvi to gitignore

--- Project1/src/run_project.py
+++ Project1/src/run_project.py
@@ -7,8 +7,6 @@ from linalg import toeplitz, thomas
 PLOTDIR = '../figures/'
 DATADIR = '../data/'
 
-n = int(sys.argv[1])
-
 def f(x):
     return 100*np.exp(-10*x)
 
@@ -20,26 +18,44 @@ def u(x):
 
 
 
-# Run thomaz algorithm
-x = np.linspace(0, 1, n+2)
-v = np.zeros(n+2)
-a = -np.ones(n)
-b = 2*np.ones(n)
-c = -np.ones(n)
-start_time = time.time()
-v[1:-1] = thomas(a, b, c, f, n)
-elapsed_time = time.time() - start_time
-with open(DATADIR + "thomas.dat", 'w') as file:
-    file.write('Run time: {}, n={}'.format(elapsed_time, n))
+ns = [10**i for i in range(1, 7)]
 
-plt.plot(x, v)
-plt.plot(x, u(x))
-plt.show()
 
-# Run algorithm for special case of a töeplitz matrix
-x = np.linspace(0, 1, n+2)
-v = np.zeros(n+2)
-v[1:-1] = toeplitz(2, f, n)
-plt.plot(x, v)
-plt.plot(x, u(x))
-# plt.show()
+with open(DATADIR + 'thomas.csv', 'w') as file:
+    file.write('n, run time\n')
+
+with open(DATADIR + 'toeplitz.csv', 'w') as file:
+    file.write('n, run time\n')
+
+
+for n in ns:
+    # Run thomaz algorithm
+    x = np.linspace(0, 1, n+2)
+    v = np.zeros(n+2)
+    a = -np.ones(n)
+    b = 2*np.ones(n)
+    c = -np.ones(n)
+    start_time = time.time()
+    v[1:-1] = thomas(a, b, c, f, n)
+    elapsed_time = time.time() - start_time
+    with open(DATADIR + "thomas.csv", 'a') as file:
+        file.write('{},{}\n'.format(n, elapsed_time))
+
+    plt.plot(x, u(x), '+')
+    plt.plot(x, v)
+    plt.savefig(PLOTDIR + "thomas_{}.png".format(n))
+    plt.clf()
+
+    # Run algorithm for special case of a töeplitz matrix
+    x = np.linspace(0, 1, n+2)
+    v = np.zeros(n+2)
+    start_time = time.time()
+    v[1:-1] = toeplitz(2, f, n)
+    elapsed_time = time.time() - start_time
+    with open(DATADIR + "toeplitz.csv", 'w') as file:
+        file.write('{},{}\n'.format(n, elapsed_time))
+
+    plt.plot(x, u(x), '+')
+    plt.plot(x, v)
+    plt.savefig(PLOTDIR + "toeplitz_{}.png".format(n))
+    plt.clf()
