c269b821a6dac32ff88106c1ceba071f74378a12
Eirik Gallefoss <>
2019-09-20 09:41:41 +0200

    Change function definitions

--- Project2/src/linalg.h
+++ Project2/src/linalg.h
@@ -1,5 +1,8 @@
 #include <armadillo>
 #include <tuple>
 using namespace arma;
-mat toeplitz(double a, double d, int N);
-void max_nondiagonal(mat A, int N, int &row, int &col);
+
+mat toeplitz(double, double, int);
+void max_nondiagonal(mat, int, int &, int &);
+void jacobi(int, double, double, double);
+void rotate(mat &, int, int, int);
