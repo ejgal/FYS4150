b96fadca213dcb45b5bb935834eef1f70b016054
Eirik Gallefoss <>
2019-09-23 12:32:44 +0200

    Experimenting with output data

--- Project2/src/post_process.py
+++ Project2/src/post_process.py
@@ -6,13 +6,31 @@ df = pd.read_csv('../data/test.dat',index_col=0, header=0)
 print(df)
 
 
-# Groupby same index
+# Groupby same index in case of data containing several runs for each n
 df = df.groupby(level=0).mean()
-print(df['iterations']/df.index**2)
 
-df['jacobi'].plot(logy=True)
+# (df['iterations']).plot()
+compare = (df['iterations']/df.index**2)
+mean = compare.mean()
+df['iterations'].plot(label="Iterations")
+plt.plot(df.index, mean*df.index**2, label="Mean iterations n^2")
+plt.legend()
+plt.show()
+
+plt.plot(mean*df.index**2 - df['iterations'])
+plt.show()
+
+# compare = (df['iterations']/df.index**3)
+# compare.plot(marker='o', linestyle='')
+# plt.axhline(compare.mean(),linestyle='--')
+# # plt.axhline(compare.mean()-compare.std(), linestyle='--')
+# # plt.axhline(compare.mean()+compare.std(), linestyle='--')
+#
+# # compare.mean().plot()
+# plt.show()
+
+# df['jacobi'].plot(logy=True)
+# plt.show()
+#
 # df['armadillo'].plot(logy=True)
-plt.show()
-
-(df['jacobi']/df['armadillo']).plot(logy=True)
-plt.show()
+# plt.show()
