3062e87556d5ffafd3a24632a3cb21864d5545c4
Eirik Gallefoss <>
2019-09-23 13:16:34 +0200

    Add some output data and figures

--- Project2/src/post_process.py
+++ Project2/src/post_process.py
@@ -1,36 +1,56 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+DATADIR = '../data/'
+PLOTDIR = '../figures/'
 
 df = pd.read_csv('../data/test.dat',index_col=0, header=0)
-print(df)
 
 
 # Groupby same index in case of data containing several runs for each n
 df = df.groupby(level=0).mean()
+print(df)
 
-# (df['iterations']).plot()
-compare = (df['iterations']/df.index**2)
-mean = compare.mean()
-df['iterations'].plot(label="Iterations")
-plt.plot(df.index, mean*df.index**2, label="Mean iterations n^2")
+
+# Number of iterations versus n
+df['iterations'].plot(loglog=True,label='Iterations')
+plt.grid()
 plt.legend()
-plt.show()
+plt.title('Iterations needed for minimizing all nondiagonal elements below 10^-8')
+plt.savefig(PLOTDIR+'iterations.png')
 
-plt.plot(mean*df.index**2 - df['iterations'])
-plt.show()
+#
+# compare = (df['iterations']/df.index**2)
+# mean = compare.mean()
+# df['iterations'].plot(label="Iterations")
+# plt.plot(df.index, mean*df.index**2, label="Mean iterations n^2")
+# plt.legend()
+# plt.show()
+
+
+
+
+
 
 # compare = (df['iterations']/df.index**3)
 # compare.plot(marker='o', linestyle='')
 # plt.axhline(compare.mean(),linestyle='--')
 # # plt.axhline(compare.mean()-compare.std(), linestyle='--')
 # # plt.axhline(compare.mean()+compare.std(), linestyle='--')
-#
+
 # # compare.mean().plot()
 # plt.show()
 
 # df['jacobi'].plot(logy=True)
 # plt.show()
 #
-# df['armadillo'].plot(logy=True)
+
+# df['jacobi'].plot()
+# df['armadillo'].plot()
+# (df['jacobi']/df['armadillo']).plot()
+plt.clf()
+(df['jacobi']/df['armadillo']).plot(loglog=True)
+plt.grid()
+plt.show()
+# plt.plot(df.index, df.index**(16/12.))
 # plt.show()
+#
