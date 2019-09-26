2bd195125d8a2ca1adaae7f06e2407e2de828d25
Ove Haugvaldstad <ovehaugv@outlook.com>
2019-09-26 13:56:10 +0200

    updated cython Jacobi

--- Project2/src/python/cythonJacobi.pyx
+++ Project2/src/python/cythonJacobi.pyx
@@ -1,85 +1,124 @@
 import numpy as np
+import timeit
 cimport numpy as cnp
-from libc.math cimport sqrt, abs
-from libc.time cimport time,time_t,clock ,clock_t
-from cython.view cimport array as cvarray
 cimport cython
+from libc.math cimport sqrt, abs
 
-cnp.import_array()
+from libc.time cimport clock ,clock_t
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-def create_toeplitz(double d,double a, int  N):
-    cdef int i
-    cdef cnp.ndarray[double, ndim=2] toeplitz = np.zeros((N,N),
-    dtype=np.float64)
-#    toeplitz = cvarray(shape=(N,N), itemsize=sizeof(double), format='d')
-    cdef double [:,:] A = toeplitz
-     
-    for i in range(N):
-        A[i,i] = d
-    for i in range(N-1):
-        A[i+1,i] = a
-        A[i, i+1] = a
-    return A
+cdef class CythonJacobi:
+    cdef double offdiagmax
+    cdef int row, col, n, iterations
+    cdef double [:,:] A
+    cdef double epsilon, 
+    
+    @cython.profile(False)
+    def __init__(self,int N ,double a, double d, double epsilon=1e-9):
+        self.row = 0
+        self.n = N
+        self.offdiagmax = 0.0
+        self.col = 0
+        self.iterations = 0
+        self.A = self.create_toeplitz(d, a, self.n)
+        self.epsilon = epsilon
+    @cython.profile(False)
+    @cython.boundscheck(False)
+    @cython.wraparound(False)    
+    cdef double [:,:] create_toeplitz(self,double d,double a, int  N):
+        cdef int i
+        cdef double [:,:] A = np.zeros((N,N), dtype=np.float64)
 
-# Trust that I have coded it correctly, set cython to not 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-def maxElemOffDiag(double [:,:] A, int n):
-    cdef double offdiagmax = 0.0
-    cdef double aij
-    cdef size_t i, j, row, col
-    for i in range(n):
-        for j in range(n):
-            aij = abs(A[i,j])
-            if (i!=j and aij >= offdiagmax):
-                offdiagmax = aij
-                row = i; col = j
+        for i in range(N):
+            A[i,i] = d
+        for i in range(N-1):
+            A[i+1,i] = a
+            A[i, i+1] = a
+        return A
 
-    return offdiagmax, row, col
+    # Trust that I have coded it correctly, set cython to not 
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    @cython.initializedcheck(False)
+    cdef void maxElemOffDiag(self,double [:,:] A, int N):
+        self.offdiagmax = 0
+        cdef int n = N
+        cdef double aij
+        cdef size_t i, j
+        for i in range(n):
+            for j in range(i,n):
+                aij = abs(A[i,j])
+                if (i!=j and aij >= self.offdiagmax):
+                    self.offdiagmax = aij
+                    self.row = i; self.col = j
+    
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    @cython.cdivision(True)
+    @cython.initializedcheck(False)
+    cdef void jacobiRotate(self,double [:,:] A,int k, int l, int n): 
+        cdef size_t i
+        cdef double tau, t, c, s, a_kk, a_ll
+        if (A[k,l] != 0.0):
+            tau = (A[l,l] - A[k,k])/(2*A[k,l])
 
-@cython.boundscheck(False)
-@cython.wraparound(False)  
-def jacobiRotate(double [:,:] A,int k, int l, int n): 
-    cdef size_t i
-    cdef double tau, t, c, s, a_kk, a_ll
-    if (A[k,l] != 0.0):
-        tau = (A[l,l] - A[k,k])/(2*A[k,l])
-
-        if(tau >= 0):
-            t = 1.0/(tau + sqrt(1.0 + tau*tau))
+            if(tau >= 0):
+                t = 1.0/(tau + sqrt(1.0 + tau*tau))
+            else:
+                t = -1.0/(-tau + sqrt(1.0 + tau*tau))
+            c = 1/sqrt(1+t*t)
+            s = c*t
         else:
-            t = -1.0/(-tau + sqrt(1.0 + tau*tau))
-        c = 1/sqrt(1+t*t)
-        s = c*t
-    else:
-        c = 1.0
-        s = 0.0
-    a_kk = A[k,k]
-    a_ll = A[l,l]
-    A[k,k] = c*c*a_kk - 2.0*c*s*A[k,l] + s*s*a_ll #Adding shit to the diagonals
-    A[l,l] = s*s*a_kk + 2.0*c*s*A[k,l] + c*c*a_ll
-    A[k,l] = 0.0  #Since our matrix is symmetric
-    A[l,k] = 0.0
-    for i in range(n):
-        if (i != k and i != l):
-            a_ik = A[i,k]
-            a_il = A[i,l]
-            A[i,k] = c*a_ik - s*a_il
-            A[k,i] = A[i,k]
-            A[i,l] = c*a_il + s*a_ik
-            A[l,i] = A[i,l]
-    return A
+            c = 1.0
+            s = 0.0
+        a_kk = A[k,k]
+        a_ll = A[l,l]
+        A[k,k] = c*c*a_kk - 2.0*c*s*A[k,l] + s*s*a_ll #Adding shit to the diagonals
+        A[l,l] = s*s*a_kk + 2.0*c*s*A[k,l] + c*c*a_ll
+        A[k,l] = 0.0  #Since our matrix is symmetric
+        A[l,k] = 0.0
+        for i in range(n):
+            if (i != k and i != l):
+                a_ik = A[i,k]
+                a_il = A[i,l]
+                A[i,k] = c*a_ik - s*a_il
+                A[k,i] = A[i,k]
+                A[i,l] = c*a_il + s*a_ik
+                A[l,i] = A[i,l]
+    
+    @cython.initializedcheck(False)
+    cdef void run(self):
+        self.iterations = 0
+        self.maxElemOffDiag(self.A, self.n)
+        while(self.offdiagmax >= self.epsilon):
+            self.iterations += 1 
+            self.jacobiRotate(self.A, self.row, self.col, self.n)
+            self.maxElemOffDiag(self.A, self.n)
 
-def jacobiRun(double[:,:] A, int N):
-    cdef int n = N
-    cdef int iterations = 0
-    cdef int row, col
-    cdef double offDiagMax = maxElemOffDiag(A, n)[0]
-    while(offDiagMax > 10e-9):
-        iterations += 1
-        offDiagMax, row, col = maxElemOffDiag(A, n)
-        A = jacobiRotate(A,row, col, n)
+    
+    cpdef void runJacobi(self):
+        self.run()
+    
+    @property
+    def getIterations(self):
+        return self.iterations
+    
+    @property 
+    def getEigenvalues(self):
+        return np.sort(np.diag(self.A))
+    
+    def calcOffDiagMax(self, double [:,:] A, int N):
+        self.maxElemOffDiag(A, N)
+        return self.offdiagmax
 
-    return A, iterations
+
+def run(N, a, d): 
+    cdef CythonJacobi run = CythonJacobi(N, a, d)
+    run.runJacobi()
+    return run.getIterations, run.getEigenvalues
+
+#This function is stupid, let it be for now to have compability with 
+#the other python programss
+def maxElemOffDiag(double [:,:] testA, int N):
+    cdef CythonJacobi maxoffdiag = CythonJacobi(10, 1, 2)
+    maxoffd = maxoffdiag.calcOffDiagMax(testA, N)
+    return maxoffd, None, None
--- Project2/src/python/test.py
+++ Project2/src/python/test.py
@@ -1,62 +1,49 @@
 import numpy as np
-import cythonJacobi
+import cythonJacobi_test
 import numbaJacobi
 import pythonJacobi
 import pytest
-
+import cythonJacobi
 
 def __calAnalyticEigenVals(d, a, N):
     x = np.zeros(N)
     for i in range(1,N+1):
         x[i-1] = d + 2*a*np.cos((i*np.pi)/(N+1))
     return x
-@pytest.mark.parametrize("prog", [cythonJacobi, numbaJacobi, pythonJacobi])
+@pytest.mark.parametrize("prog", [cythonJacobi_test, numbaJacobi, pythonJacobi])
 def test_MaxElemOffDiag(prog):
 
     np.random.seed(126)
 
     rand_mat = np.random.randint(1,100,(4,4)).astype(float)
     rand_mat[3,3] = 99 #Diagonal element has the highest values, 
-    rand_mat[3,2] = 98
+    rand_mat[2,3] = 98
     N = rand_mat.shape[0]
     assert prog.maxElemOffDiag(rand_mat,N)[0] == 98
-@pytest.mark.parametrize("prog", [cythonJacobi, numbaJacobi, pythonJacobi])
-def test_rotation(prog):
 
-    A = np.array([[3,2,4,5],
-                    [2,5,3,4],
-                    [4,3,2,6],
-                    [5,4,6,2]]).astype(float)
-    Before = np.copy(A)[0:2,0:2]
-    N = A.shape[0]
-    RotA = prog.jacobiRotate(A,3,2,N)
-    Unchanged = RotA[0:2,0:2]
-    for i in range(2):
-        for j in range(2):
-            assert Before[i,j] == Unchanged[i,j]
+testsetup = [(cythonJacobi_test, 200, True),
+             (numbaJacobi, 200, False),
+              (pythonJacobi, 20, False)]
 
-testsetup = [(cythonJacobi, 200),
-             (numbaJacobi, 200),
-              (pythonJacobi, 20)]
-
-@pytest.mark.parametrize("prog, N" , testsetup)
-def test_Eigenvalues(prog, N):
+@pytest.mark.parametrize("prog, N, Cython" , testsetup)
+def test_Eigenvalues(prog, N, Cython):
     N = N
 
     h = 1./(N)
     d = 2./(h**2)
     a = -1./(h**2)
     eigValsAnalytic =  __calAnalyticEigenVals(d, a, N)
-
-    A = prog.create_toeplitz(d, a, N)
-    offDiagMax = prog.maxElemOffDiag(A,N)[0]
-    iterations = 0
-    while(offDiagMax > 1e-9):
-        iterations += 1
-        offDiagMax, row, col = prog.maxElemOffDiag(A, N)
-        A =prog.jacobiRotate(A,row, col, N)
-    eigValsAnalytic = __calAnalyticEigenVals(d, a, N)
-
-    assert np.allclose(np.sort(np.diag(A), eigValsAnalytic)
-
-
+    if Cython:
+        calcEigVals = np.sort(prog.run(N, a, d)[1])
+    else:
+        A = prog.create_toeplitz(d, a, N)
+        offDiagMax = prog.maxElemOffDiag(A,N)[0]
+        iterations = 0
+        while(offDiagMax > 1e-9):
+            iterations += 1
+            offDiagMax, row, col = prog.maxElemOffDiag(A, N)
+            A =prog.jacobiRotate(A,row, col, N)
+        calcEigVals = np.sort(np.diag(A))
+    
+    assert np.allclose(calcEigVals, eigValsAnalytic)
+    
