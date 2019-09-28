78888e2d21ff10dec1dcc654ce60140242f180e9
Ove Haugvaldstad <ovehaugv@outlook.com>
2019-09-28 17:47:01 +0200

    changed names

--- Project2/src/python/timeFastPython.py
+++ Project2/src/python/timeFastPython.py
@@ -3,15 +3,15 @@ import cythonJacobi
 import numbaJacobi
 
 
-Ns = [100, 120, 150, 180, 200, 220, 250, 275, 300]
+Ns = [100, 120, 150, 180, 200, 220, 250, 275, 300, 350]
 runs = 5
 
-with open('../../data/fastpythonRuns.dat','w') as file:
+with open('../../data/fpython_100_350.dat','w') as file:
     file.write('n, numba, cython\n')
 for i in range(len(Ns)):
 #     dataPython[i,0]
     timeCython = cythonJacobi.timeJacobi(Ns[i], runs)[2]
     timeNumba = numbaJacobi.timeJacobi(Ns[i], runs)[2]
-    with open('../../data/fastpythonRuns.dat','a') as file:
+    with open('../../data/fpython_100_350.dat','a') as file:
         for j in range(len(timeNumba)):
             file.write('{},{:.8f},{:.8f} \n'.format(Ns[i],timeNumba[j], timeCython[j]))
