7f8d8dffb475c2b6104190bd8ab0a8799f8959f6
Eirik Gallefoss <>
2019-10-17 11:02:54 +0200

    Store more data when running experiment

--- Project3/src/python/run_montecarlo.py
+++ Project3/src/python/run_montecarlo.py
@@ -21,6 +21,8 @@ parser.add_argument('--exp', type=int, default=exp_default, help=exp_help)
 
 
 if __name__ == '__main__':
+
+    # Run parser and store input to variables
     args = parser.parse_args()
     exp = args.exp
     output = args.output
@@ -30,6 +32,7 @@ if __name__ == '__main__':
     Ns = [10**i for i in range(1,exp+1)]
     runs = len(Ns)
 
+    # Arrays for storing experiment data
     std_brute = np.zeros(runs)
     error_brute = np.zeros(runs)
     std_importance = np.zeros(runs)
@@ -37,6 +40,8 @@ if __name__ == '__main__':
     time_brute = np.zeros(runs)
     time_importance = np.zeros(runs)
     time_importance_parallel = np.zeros(runs)
+    result_brute = np.zeros(runs)
+    result_importance = np.zeros(runs)
 
     for N,i in zip(Ns,range(0,runs)):
         N = int(N)
@@ -49,6 +54,7 @@ if __name__ == '__main__':
         std_brute[i] = np.sqrt(mc.variance(s1,s2,N))
         error_brute[i] = analytical - s1
         time_brute[i] = end-start
+        result_brute[i] = s1
 
         # Run monte-carlo with importance sampling (unparallellized)
         start = time.time()
@@ -57,6 +63,7 @@ if __name__ == '__main__':
         std_importance[i] = np.sqrt(mc.variance(s1,s2,N))
         error_importance[i] = analytical - s1
         time_importance[i] = end-start
+        result_importance[i] = s1
 
         # Run monte-carlo with importance sampling (unparallelized)
         start = time.time()
@@ -64,61 +71,11 @@ if __name__ == '__main__':
         end = time.time()
         time_importance_parallel[i] = end-start
 
-    # print(std_brute)
-    # print(std_importance)
-    # print((error_brute - error_importance))
-    # print((std_brute - std_importance))
 
-    #
-    data = np.array([std_brute, std_importance, error_brute, error_importance, time_brute, time_importance, time_importance_parallel]).transpose()
-    columns = ['std_brute','std_importance','error_brute','error_importance','time_brute','time_importance', 'time_importance_parallel']
+    # Store data to csv file
+    data = np.array([result_brute, result_importance, std_brute, std_importance, error_brute, error_importance, time_brute, time_importance, time_importance_parallel]).transpose()
+    columns = ['result_brute','result_importance','std_brute','std_importance','error_brute','error_importance','time_brute','time_importance', 'time_importance_parallel']
     index = Ns
     df = pd.DataFrame(data=data,index=index, columns=columns)#, columns=columns)
     df.index.name='N'
     df.to_csv(output)
-
-        # mc.print_results(s1,s2,N)
-
-        # s1,s2 = montecarlo_importance(N)
-        # print_results(s1,s2)
-    # print(std)
-    # plt.plot(Ns, std)
-    # plt.xscale("log")
-    # plt.show()
-    #
-    # print(error)
-    #
-    # plt.plot(Ns, np.abs(error))
-    # plt.xscale("log")
-    # plt.show()
-
-
-
-# import argparse
-#
-# parser = argparse.ArgumentParser(description='Calculates height of ball.')
-#
-# v_low = 0
-# v_high = 500
-# v_meta = '[{}-{}]'.format(v_low, v_high)
-#
-# parser.add_argument('--v', '--initial_velocity',
-#                     choices=[i for i in range(v_low,v_high)],
-#                     metavar='[{}-{}]'.format(v_low, v_high),
-#                     type=float, default=30,
-#                     help=' | m/s')
-#
-# parser.add_argument('--t', '--time',
-#                     type=float,
-#                     default=10,
-#                     help=' | s')
-#
-# parser.add_argument('--g', '--gravity',
-#                     type=float,
-#                     default=9.81,
-#                     help=' | m/s**2')
-# g = args.g
-# v0 = args.v
-# t = args.t
-#
-# print v0*t - 0.5*g*t**2
