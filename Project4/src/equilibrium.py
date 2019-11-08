14fed75c2ff0d25b8f01123467d90f49fe4921b9
Eirik Gallefoss <>
2019-11-08 14:43:01 +0100

    Add new data, figures + new plot function

--- Project4/src/equilibrium.py
+++ Project4/src/equilibrium.py
@@ -4,7 +4,7 @@ import argparse
 
 from ising import ising, expectation_values, write_run, write_header
 from parser import equi_parser
-from plot import *
+from settings import *
 
 
 DATADIR = '../data/'
@@ -12,20 +12,23 @@ DATADIR = '../data/'
 if __name__ == '__main__':
     # Run parser and store input to variables
     args = equi_parser().parse_args()
-    exp = args.exp
+    exp = int(args.exp)
     output = args.output
-    points = args.points
-    ordered = args.ordered
+    points = int(args.points)
+
+    # Ignore ordered input...
+    # ordered = int(args.ordered)
 
     L = 20
     N = points
-    cycles = np.logspace(2,6,N)
+    cycles = np.logspace(2,exp,N)
     delay = 0
 
-    write_header(filename)
+    write_header(output)
     for T in [1., 2.4]:
-        for i in range(0,len(cycles)):
-            print('T: {}, cycle: {}/{}'.format(T, int(cycles[i]), cycles[-1]))
-            values, accepted = ising(L,cycles[i],T, delay=0, ordered=0)
-            expect = expectation_values(values, cycles[i],L,T)
-            write_run(filename, expect, T,L,ordered,cycles[i],delay,accepted)
+        for ordered in [-1,0,1]:
+            for i in range(0,len(cycles)):
+                print('T: {}, cycle: {}/{}'.format(T, int(cycles[i]), cycles[-1]))
+                values, accepted = ising(L,cycles[i],T, delay=0, ordered=0)
+                expect = expectation_values(values, cycles[i],L,T)
+                write_run(output, expect, T,L,ordered,cycles[i],delay,accepted)
--- Project4/src/postprocess.py
+++ Project4/src/postprocess.py
@@ -5,18 +5,23 @@ import numpy as np
 from settings import *
 from parser import post_parser
 
-def plot_equilibrium(datafile):
-    df = pd.read_csv(datafile)
-    df['E'] = np.abs(df['E'])
-    df['M'] = np.abs(df['M'])
-    for col in ['E','M']:
-        fig,ax = plt.subplots(2)
-        ax1 = ax[0]
-        ax2 = ax[1]
-        df.loc[df['T']==1.0].plot('cycles',col, ax=ax1,logx=True,label='|{}|, T=1.0'.format(col))
-        df.loc[df['T']==2.4].plot('cycles',col, ax=ax2, logx=True,label='|{}|, T=2.4'.format(col))
-        plt.savefig(FIGDIR + 'equilibrium_{}'.format(col))
-        plt.clf()
+# def plot_equilibrium(datafile):
+#     df = pd.read_csv(datafile)
+#     df['E'] = np.abs(df['E'])
+#     df['M'] = np.abs(df['M'])
+#     for col in ['E','M']:
+#         fig,ax = plt.subplots(2)
+#         ax1 = ax[0]
+#         ax2 = ax[1]
+#         label1 = r'$<\vert$ {} $\vert >$\, T=1.0'.format(col)
+#         label2 = r'$<\vert$ {} $\vert >$\, T=2.4'.format(col)
+#         # df.loc[(df['T']==1.0) & (df['ordered']==1)].plot('cycles',col, ax=ax1, label=label1,logx=True)
+#         df.loc[(df['T']==2.4) & (df['ordered']==1)].plot('cycles',col, ax=ax1, label=label2,logx=True)
+#
+#         # df.loc[(df['T']==1.0) & (df['ordered']==1)].plot('cycles',col, ax=ax1,logx=True,label=label1)
+#         # df.loc[(df['T']==2.4]) & (df['ordered']==1)].plot('cycles',col, ax=ax1, logx=True,label='|{}|, T=2.4'.format(col))
+#         plt.savefig(FIGDIR + 'equilibrium_{}'.format(col))
+#         plt.clf()
 
 def plot_phase(datafile):
     df = pd.read_csv(datafile)
@@ -27,6 +32,9 @@ def plot_phase(datafile):
             label = 'L={}'.format(L)
             df.loc[df['spins']==spins].plot('T',col, ax=ax,linestyle=' ',markersize=1,marker='o',label=label)
         ax.legend()
+        # Plot critical value
+        # plt.axvline(2.269)
+
         plt.savefig(FIGDIR + 'phase_{}.png'.format(col))
         plt.clf()
 
@@ -35,5 +43,27 @@ if __name__ == '__main__':
     equi = args.equi
     phase = args.phase
 
+
+    # Plot equilibrium tests
+    df = pd.read_csv(equi)
+    df['E'] = np.abs(df['E'])
+    df['M'] = np.abs(df['M'])
+    for col in ['E','M']:
+        fig, ax=plt.subplots(3, sharex=True)
+        for T in [1.0,2.4]:
+            lb = 'T={}'.format(T)
+            for axis, ordered in zip(ax, [1,-1,0]):
+                df.loc[(df['T']==T) & (df['ordered']==ordered)].plot('cycles',col, ax=axis,linestyle='--', label=lb,logx=True)
+                axis.set_ylabel('{} - ordered={}'.format(col,ordered))
+        plt.savefig(FIGDIR + 'equilibrium_{}.png'.format(col))
+
+
+    # df.loc[(df['T']==1.0) & (df['ordered']==1)].plot('cycles','M', ax=ax1, label=lb1,logx=True)
+    # df.loc[(df['T']==2.4) & (df['ordered']==1)].plot('cycles','M', ax=ax1, label=lb2,logx=True)
+    # df.loc[(df['T']==1.0) & (df['ordered']==0)].plot('cycles','M', ax=ax2, label=lb1,logx=True)
+    # df.loc[(df['T']==2.4) & (df['ordered']==0)].plot('cycles','M', ax=ax2, label=lb2,logx=True)
+    # df.loc[(df['T']==1.0) & (df['ordered']==-1)].plot('cycles','M', ax=ax3, label=lb1,logx=True)
+    # df.loc[(df['T']==2.4) & (df['ordered']==-1)].plot('cycles','M', ax=ax3, label=lb2,logx=True)
+
+    # plot_equilibrium(DATADIR + equi)
     plot_phase(DATADIR + phase)
-    plot_equilibrium(DATADIR + equi)
