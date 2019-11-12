cf5a96d89b168069d63270a0999ed1d63d90c3b5
Eirik Gallefoss <>
2019-11-12 17:59:44 +0100

    Forgot to pass order to isin in equilibrium. Turned on -> new plots

--- Project4/src/equilibrium.py
+++ Project4/src/equilibrium.py
@@ -26,6 +26,6 @@ if __name__ == '__main__':
         for ordered in [-1,0,1]:
             for i in range(0,len(cycles)):
                 print('T: {}, cycle: {}/{}'.format(T, int(cycles[i]), cycles[-1]))
-                values, accepted,d = ising(L,int(cycles[i]),T, delay=0, ordered=0)
+                values, accepted,d = ising(L,int(cycles[i]),T, delay=0, ordered=ordered)
                 expect = expectation_values(values, cycles[i],L,T)
                 write_run(output, expect, T,L,ordered,cycles[i],delay,accepted)
