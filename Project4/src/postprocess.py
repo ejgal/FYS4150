beca1d0f6cdf4a83eb4c633e833bb5a9d262f0b1
Eirik Gallefoss <>
2019-11-12 18:00:21 +0100

    Small changes grids and cycles

--- Project4/src/postprocess.py
+++ Project4/src/postprocess.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.ticker import (AutoMinorLocator, MultipleLocator)
 
 from settings import *
 from parser import post_parser
@@ -12,14 +13,19 @@ def plot_equilibrium(datafile):
     df = pd.read_csv(equi)
     df['E'] = np.abs(df['E'])
     df['M'] = np.abs(df['M'])
+    ticks = np.arange(-5,5,0.1)
     for col in ['E','M']:
-        fig, ax=plt.subplots(3, sharex=True)
+        fig, ax=plt.subplots(2, sharex=False)
         for T in [1.0,2.4]:
             lb = 'T={}'.format(T)
-            for axis, ordered in zip(ax, [1,-1,0]):
+            # Plot for spins up and random
+            for axis, ordered in zip(ax, [1,0]):
                 sel = df.loc[(df['T']==T) & (df['ordered']==ordered)]
-                sel.plot('cycles',col, ax=axis,linestyle='--', label=lb,logx=True)
+                sel = sel.loc[df['cycles'].between(0,100000)]
+                sel.plot('cycles',col, ax=axis,linestyle='-', label=lb)#,logx=True)
                 axis.set_ylabel(r'$\vert${}$\vert$ ord={}'.format(col,ordered))
+                axis.yaxis.set_major_locator(MultipleLocator(0.1))
+                axis.grid(linestyle='--',axis='y')
         plt.savefig(FIGDIR + 'equilibrium_{}.png'.format(col))
         plt.clf()
 
@@ -36,6 +42,7 @@ def plot_distribution(distfile, datafile):
         label = 'T={} C$_v$={:.2f}'.format(T,data.loc[float(T),'cv'])
         dist[T].hist(density=True, bins=num_bins,label=label, alpha=0.8)
     plt.legend()
+    plt.grid(False)
     plt.ylabel('Probability density')
     plt.xlabel('Energy')
     plt.savefig(FIGDIR + 'distribution.png')
@@ -48,7 +55,7 @@ def plot_phase(datafile):
         for L in [40,60,80,100]:
             spins = L**2
             label = 'L={}'.format(L)
-            df.loc[df['spins']==spins].plot('T',col, ax=ax,linestyle=' ',markersize=1,marker='o',label=label)
+            df.loc[df['spins']==spins].plot('T',col, ax=ax,linestyle=' ',markersize=1.5,marker='o',label=label)
         ax.legend()
         ax.set_ylabel(ylabel)
         plt.savefig(FIGDIR + 'phase_{}.png'.format(col))
@@ -58,10 +65,11 @@ def plot_accepted(datafile):
     df = pd.read_csv(datafile)
     df['ratio'] = df['accepted']/(df['spins']*df['cycles'])
     fig, ax = plt.subplots(1)
+    # df = df.loc[df['cycles'].between(0,100000)]
     df.loc[(df['T']==1.0) & (df['ordered']==0)].plot('cycles','ratio',ax=ax,label='T=1.0')
     df.loc[(df['T']==2.4) & (df['ordered']==0)].plot('cycles','ratio',ax=ax,label='T=2.4')
     plt.xscale('log')
-    plt.yscale('log')
+    # plt.yscale('log')
     plt.grid()
     plt.legend()
     plt.ylabel('Accepted / (spins $\cdot$ cycles)')
@@ -76,7 +84,7 @@ if __name__ == '__main__':
     distfile = args.distfile
     datafile = args.distdata
 
+    plot_equilibrium(equi)
     plot_accepted(equi)
     plot_distribution(distfile, datafile)
-    plot_equilibrium(equi)
     plot_phase(phase)
