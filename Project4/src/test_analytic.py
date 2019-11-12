bc3d9ffb76da94728c4de9f6b74856bcfec1345c
Eirik Gallefoss <>
2019-11-12 09:02:49 +0100

    Update tests

--- Project4/src/test_analytic.py
+++ Project4/src/test_analytic.py
@@ -5,22 +5,23 @@ import analytic as a
 
 
 
+tol = 10**-12
+
+
 def test_z():
-    for T in np.linspace(1.,10.,5):
+    for T in np.linspace(1., 10., 5):
         analytic = a.z(T)
         calculated = 0
         states = a.produce_states()
         for state in states:
             energy = a.energy(state)
             calculated += np.exp(-1/T*energy)
-        assert analytic == approx(calculated)
+        assert analytic == approx(calculated, abs=tol)
 
 
 def test_expected_energy():
     "<E>"
     for T in np.linspace(1.,10.,5):
-        zt = a.z(T)
-        B=1/T
         analytic = a.expected_energy(T)
         states = a.produce_states()
         calculated = 0
@@ -28,7 +29,7 @@ def test_expected_energy():
             energy = a.energy(state)
             prob = a.probability(T,state)
             calculated += prob*energy
-        assert analytic == approx(calculated)
+        assert analytic == approx(calculated, abs=tol)
 
 def test_E2():
     "<E^2>"
@@ -41,7 +42,7 @@ def test_E2():
             energy = a.energy(state)
             prob = a.probability(T,state)
             calculated += prob*energy**2
-        assert analytic == approx(calculated)
+        assert analytic == approx(calculated, abs=tol)
 
 
 def test_Esquared():
@@ -49,7 +50,6 @@ def test_Esquared():
     for T in np.linspace(1.,10.,3):
         B = 1/T
         zt = a.z(T)
-        # analytic = (16/a.z(T))**2 *(np.exp(-16*B) + np.exp(16*B) - 2)
         analytic = 32**2/(zt)**2 * np.sinh(8*B)**2
         states = a.produce_states()
         calculated = 0
@@ -77,7 +77,7 @@ def test_M():
             magnetization = a.magnetization(state)
             prob = a.probability(T,state)
             calculated += np.abs(magnetization)*prob
-        assert analytic == approx(calculated)
+        assert analytic == approx(calculated, abs=tol)
 
 
 def test_M2():
@@ -93,7 +93,7 @@ def test_M2():
             magnet = np.abs(a.magnetization(state))
             prob = a.probability(T,state)
             calculated += prob*magnet**2
-        assert analytic == approx(calculated)
+        assert analytic == approx(calculated, abs=tol)
 
 
 def test_Msquared():
@@ -109,7 +109,7 @@ def test_Msquared():
             magnet = np.abs(a.magnetization(state))
             prob = a.probability(T,state)
             calculated += prob*magnet
-        assert analytic == approx(calculated**2)
+        assert analytic == approx(calculated**2, abs=tol)
 
 
 
@@ -126,7 +126,7 @@ def test_variance_energy():
             prob = a.probability(T,state)
             E2 += energy**2*prob
             E +=  energy*prob
-        assert analytic == approx(E2 - E**2)
+        assert analytic == approx(E2 - E**2, abs=tol)
 
 def test_variance_magnetization():
     for T in np.linspace(1.,10.,5):
@@ -139,4 +139,4 @@ def test_variance_magnetization():
             prob = a.probability(T,state)
             M2 += magnetization**2*prob
             M += magnetization*prob
-        assert analytic == approx(M2 - M**2)
+        assert analytic == approx(M2 - M**2, abs=tol)
--- Project4/src/test_ising.py
+++ Project4/src/test_ising.py
@@ -14,6 +14,7 @@ def test_ising_EM():
     spins = L**2
     cycles = 100000
     tol = 0.001
+    delay = 0
     T=1
     for i in range(0,10):
         values,acc,d = ising(L,cycles,T)
@@ -39,10 +40,11 @@ def test_ising_cv_suscept():
     spins = L**2
     cycles = 5000000
     tol = 0.05
+    delay = 0
     T=1
     for i in range(0,10):
-        values,acc,d = ising(L,cycles,T)
-        expect = expectation_values(values, cycles,L,T)
+        values,acc,d = ising(L,cycles,T, delay)
+        expect = expectation_values(values, cycles,L,T, delay)
 
         # Analytical solutions
         cv = a.cv(T)/spins
@@ -52,18 +54,18 @@ def test_ising_cv_suscept():
         assert cv == approx(expect[3], rel=tol)
         assert suscept == approx(expect[4], rel=tol)
 
-# def test_jit():
-#     """
-#     Check that ising with and without numba gives close to the same
-#     values. Uses rel=0.001 since we did not want to clutter the ising
-#     function more by having an option to set a seed in the RNG.
-#     Does not test the magnetization since it has very high variance.
-#     """
-#
-#     nojit,a,d = ising.py_func(2,100000,1)
-#     jit,a,d = ising(2,100000,1)
-#     for value1, value2 in zip(nojit[[0,1,3,4]], jit[[0,1,3,4]]):
-#         assert value1 == approx(value2, rel=0.001)
-#
-# if __name__ == '__main__':
-#     test_jit()
+def test_jit():
+    """
+    Check that ising with and without numba gives close to the same
+    values. Uses rel=0.001 since we did not want to clutter the ising
+    function more by having an option to set a seed in the RNG.
+    Does not test the magnetization since it has very high variance.
+    """
+
+    nojit,a,d = ising.py_func(2,100000,1)
+    jit,a,d = ising(2,100000,1)
+    for value1, value2 in zip(nojit[[0,1,3,4]], jit[[0,1,3,4]]):
+        assert value1 == approx(value2, rel=0.001)
+
+if __name__ == '__main__':
+    test_jit()
