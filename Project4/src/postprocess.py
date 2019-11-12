e6355498dd3544348323af9facf0419efb90164d
Eirik Gallefoss <>
2019-11-12 19:16:53 +0100

    Dont need as many MC cycles for equilibrium as thought. New plots and data for fewer runs.

--- Project4/src/postprocess.py
+++ Project4/src/postprocess.py
@@ -12,16 +12,16 @@ def plot_equilibrium(datafile):
     # Plot equilibrium tests
     df = pd.read_csv(equi)
     df['E'] = np.abs(df['E'])
-    df['M'] = np.abs(df['M'])
+    df['Mabs'] = np.abs(df['Mabs'])
     ticks = np.arange(-5,5,0.1)
-    for col in ['E','M']:
+    for col in ['E','Mabs']:
         fig, ax=plt.subplots(2, sharex=False)
         for T in [1.0,2.4]:
             lb = 'T={}'.format(T)
             # Plot for spins up and random
             for axis, ordered in zip(ax, [1,0]):
                 sel = df.loc[(df['T']==T) & (df['ordered']==ordered)]
-                sel = sel.loc[df['cycles'].between(0,100000)]
+                sel = sel.loc[df['cycles'].between(0,50000)]
                 sel.plot('cycles',col, ax=axis,linestyle='-', label=lb)#,logx=True)
                 axis.set_ylabel(r'$\vert${}$\vert$ ord={}'.format(col,ordered))
                 axis.yaxis.set_major_locator(MultipleLocator(0.1))
@@ -55,7 +55,7 @@ def plot_phase(datafile):
         for L in [40,60,80,100]:
             spins = L**2
             label = 'L={}'.format(L)
-            df.loc[df['spins']==spins].plot('T',col, ax=ax,linestyle=' ',markersize=1.5,marker='o',label=label)
+            df.loc[df['spins']==spins].plot('T',col, ax=ax,linestyle='-',markersize=1.5,marker='o',label=label)
         ax.legend()
         ax.set_ylabel(ylabel)
         plt.savefig(FIGDIR + 'phase_{}.png'.format(col))
